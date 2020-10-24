58330ed6868a286a191bcbf4e8bf56157a1a42bd
Abhishek Kumar Sharma <theabhishek.aks22@gmail.com>
2020-10-24 15:39:36 +0530

    [Abhishek] Add . Added to the UC4 branch

--- StackQueue/Program.cs
+++ StackQueue/Program.cs
@@ -10,6 +10,7 @@ namespace StackQueue
             queue.Enqueue(56);
             queue.Enqueue(30);
             queue.Enqueue(70);
+            queue.Dequeue();
             queue.DisplayQueue();
         }
     }
